24ac32c0b392d7b2cbdb3c7c3468ad6d2d106b7b
Maxime <maxime.lombardo@gmail.com>
2020-01-30 08:59:08 +0100

    ajout couleur cadre

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // ---------------------------------------------------------------------------------
 
 app.get("/api/show", (req, res) => {
