74ead6c56a0d972f1eb49db9891f114bb9735fa5
Maxime <maxime.lombardo@gmail.com>
2020-02-02 15:42:21 +0100

    variable denvironnement ok

--- src/config.js
+++ src/config.js
@@ -1,9 +1,12 @@
 const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Mldlp2015?",
-  database: "checkpoint"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 module.exports = connection;
