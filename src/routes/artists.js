159738fc0a22b7da18de2a644d571114a48aad64
Maxime <maxime.lombardo@gmail.com>
2020-02-03 16:43:53 +0100

    pret pour la demo

--- src/routes/artists.js
+++ src/routes/artists.js
@@ -5,6 +5,23 @@ const connection = require("../config");
 
 // -----------------GET ALL ARTIST----------------------------------------------------------------
 
+router.get("/", (req, res) => {
+  const idQuery = req.params.id;
+  connection.query(
+    "SELECT * FROM artist ORDER BY firstname",
+    idQuery,
+    (err, results) => {
+      if (err) {
+        res.status(500).send("Error retrieving artist");
+      } else {
+        res.json(results);
+      }
+    }
+  );
+});
+
+// -----------------GET ARTIST BY SHOW_ID----------------------------------------------------------------
+
 router.get("/:id", (req, res) => {
   const idQuery = req.params.id;
   connection.query(
@@ -18,6 +35,32 @@ router.get("/:id", (req, res) => {
       }
     }
   );
+});
+
+// -----------------POST AN ARTIST----------------------------------------------------------------
+
+router.post("/", (req, res) => {
+  const formaData = req.body;
+  connection.query("INSERT INTO artist SET ?", formaData, (err, results) => {
+    if (err) {
+      res.status(500).send("Error saving artist");
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// -----------------DELETE AN ARTIST----------------------------------------------------------------
+
+router.delete("/:id", (req, res) => {
+  const idUrl = req.params.id;
+  connection.query("DELETE FROM artist WHERE id = ?", [idUrl], err => {
+    if (err) {
+      res.status(500).send("Error deleting");
+    } else {
+      res.sendStatus(200);
+    }
+  });
 });
 
 module.exports = router;
