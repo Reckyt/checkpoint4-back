246d2d25bcbe4a7d105a401f3dbab447532c60c9
Maxime <maxime.lombardo@gmail.com>
2020-03-16 17:36:28 +0100

    fin

--- index.js
+++ index.js
@@ -4,24 +4,13 @@ const bodyParser = require("body-parser");
 const api = require("./src/routes");
 
 const app = express();
-<<<<<<< HEAD
-const port = 3000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-=======
 const PORT = 3000;
-
-const bodyParser = require("body-parser");
-const api = require("./src/routes");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api", api);
 
->>>>>>> 4cc3b243ec8af3d5c0a965b37a719aebe7b8cfa1
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -31,19 +20,9 @@ app.use(function(req, res, next) {
   next();
 });
 
-<<<<<<< HEAD
-app.use("/api", api);
-
-app.listen(port, err => {
-  if (err) {
-    throw new Error("There is an error");
-  }
-  console.log("ouverture du port");
-=======
 app.listen(PORT, err => {
   if (err) {
     throw new Error("There is an error");
   }
   console.log("le port est ouvert");
->>>>>>> 4cc3b243ec8af3d5c0a965b37a719aebe7b8cfa1
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
 const artists = require("./artists");
 const shows = require("./shows");
 const command = require("./command");
@@ -9,14 +8,5 @@ const router = express.Router();
 router.use("/artists", artists);
 router.use("/shows", shows);
 router.use("/command", command);
-=======
-const user = require("./user");
-const artist = require("./artist");
-
-const router = express.Router();
-
-router.use("/artist", artist);
-router.use("/user", user);
->>>>>>> 4cc3b243ec8af3d5c0a965b37a719aebe7b8cfa1
 
 module.exports = router;
